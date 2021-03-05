2054537890f7e5917ab3710e55a7ef6ef72b3c07
Jeffrey Szymanski <jeszyman@gmail.com>
2021-03-05 13:35:06 -0600

    2021-03-05

--- src/functions.sh
+++ src/functions.sh
@@ -19,39 +19,30 @@ docker_interactive() {
                                       --volume /tmp/:/tmp/ \
                                       $account/$container \
                                       /bin/bash;;
-            (jeszyman-*) docker run -it \
-                                --env HOME=/home/jeszyman \
-                                --hostname ${HOSTNAME} \
-                                --user $(id -u ${USER}) \
-                                --volume /home/:/home/ \
-                                --volume /mnt/:/mnt/ \
-                                --volume /tmp/:/tmp/ \
-                                $account/$container \
-                                /bin/bash;;
-        (jeff-mac*) docker run -it \
-                            --env HOME=/home/jeszyman \
-                            --hostname ${HOSTNAME} \
-                            --user $(id -u ${USER}) \
-                            --volume /home/:/home/ \
-                            --volume /mnt/:/mnt/ \
-                            --volume /tmp/:/tmp/ \
-                            jeszyman/$container \
-                            /bin/bash;;        
-        (acl*) docker run -it \
-                      -v /drive3/:/drive3/ \
-                      -v /duo4/:/duo4/ \
-                      -v /home/:/home/ \
-                      -u $(id -u ${USER}) \
-                      jeszyman/$container \
-                      /bin/bash;;
-        (ACL*) docker run -it \
-                      -v /home/:/home/ \
-                      -v /duo4/:/duo4/ \
-                      -u $(id -u ${USER}):$(id -g ${USER}) \
-                      -h=${HOSTNAME} \
-                      jeszyman/$container \
-                      /bin/bash;;
-        (virtual-workstation*.gsc.wustl.edu) bsub -Is -q docker-interactive -a 'docker(jeszyman/'"$container"')' /bin/bash;;
+            (acl*) docker run -it \
+                          -v /drive3/:/drive3/ \
+                          -v /duo4/:/duo4/ \
+                          -v /home/:/home/ \
+                          -u $(id -u ${USER}) \
+                          $account/$container \
+                          /bin/bash;;
+            (ACL*) docker run -it \
+                          -v /home/:/home/ \
+                          -v /duo4/:/duo4/ \
+                          -u $(id -u ${USER}):$(id -g ${USER}) \
+                          -h=${HOSTNAME} \
+                          $account/$container \
+                          /bin/bash;;
+            (virtual-workstation*.gsc.wustl.edu) bsub -Is -q docker-interactive -a 'docker($account/'"$container"')' /bin/bash;;
+            (*) docker run -it \
+                       --env HOME=/home/${USER} \
+                       --hostname ${HOSTNAME} \
+                       --user $(id -u ${USER}) \
+                       --volume /home/:/home/ \
+                       --volume /mnt/:/mnt/ \
+                       --volume /tmp/:/tmp/ \
+                       $account/$container \
+                       /bin/bash;;
         esac
     fi
 }
