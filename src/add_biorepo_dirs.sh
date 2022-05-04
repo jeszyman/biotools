e637faad1d00dbc2e06d6282ccfbfb5ad0e36d2c
Jeffrey Szymanski <jeszyman@gmail.com>
2022-05-04 08:22:27 -0500

    /home/jeszyman/repos/biotools

--- src/add_biorepo_dirs.sh
+++ src/add_biorepo_dirs.sh
@@ -1,3 +1,10 @@
+# Check for parameters, return usage if empty
+if [[ $# -eq 0 ]] || [[ add_biorepo_dirs.sh == "h" ]] ; then
+    printf "\n usage: add_biorepo_dirs.sh repo  
+           \n Creates common repository directories for a biopipe as needed
+           \n repo: Path to local repository base directory
+           \n "
+else
 repo_dirs=("config"
            "resources"
            "src"
@@ -8,3 +15,4 @@ for dir in "${repo_dirs[@]}";
 do 
    mkdir -p $1/$dir
 done
+fi
