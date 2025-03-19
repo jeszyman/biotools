11f970824eb96ec3c10956915876b615db3ca5d0
Jeffrey Szymanski <jeszyman@gmail.com>
2025-03-19 15:52:19 -0500

    .

--- workflow/scripts/make_bwa_index_.sh
+++ workflow/scripts/make_bwa_index_.sh
@@ -6,12 +6,3 @@ bwa index -p $2 -a bwtsw $3
 # Snakemake variables
 # Function
 # Run command
-
-
-# - [[file:./workflow/scripts/make_bwa_index_.sh][Base script]]
-
-mkdir -p $1
-bwa index -p $2 -a bwtsw $3
-# Snakemake variables
-# Function
-# Run command
